9655971c4cb13bc8009a85765e33e64d9e88dfb7
hpeng526 <hpeng526@gmail.com>
2021-07-29 22:18:24 +0800

    Initial Commit

---
+++ ddcgui/Views/Presenter.swift
@@ -0,0 +1,8 @@
+//
+//  Presenter.swift
+//  ddcgui
+//
+//  Created by Peng Huang on 2021/7/29.
+//
+
+import Foundation
---
+++ ddcgui/ddcguiApp.swift
@@ -0,0 +1,17 @@
+//
+//  ddcguiApp.swift
+//  ddcgui
+//
+//  Created by Peng Huang on 2021/7/29.
+//
+
+import SwiftUI
+
+@main
+struct ddcguiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
